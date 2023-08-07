8c210a4360175f8147c5a6e237973a2045f3750a
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-08-08 01:07:23 +0200

    sstarting with second page after data code is done

--- profilePage.js
+++ profilePage.js
@@ -7,52 +7,64 @@ var area = $('#area').val();
 var email = $('#email').val();
 var country = $("#country").val();
 var stateRegion = $("#stateRegion").val()
-$("#form").on("submit", function {
-    event.preventDefault();
-    if (isEmpty()) {
-
+$('#password').on("keyup", function() {
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    
+    if (password.match(lowerCaseLetters)) {
+        $('#letter').removeClass("invalid").addClass("valid");
+    } else {
+        $('#letter').removeClass("valid").addClass("invalid");
     }
-})
-function isEmpty(){
-    if (firstName === "" || lastName === "" || mobileNumber === "" || postCode === "" || area === "" || email === "" || country === "" ||) {
-        alert('Please fill out all required fields');
+    
+    if (password.match(upperCaseLetters)) {
+        $('#capital').removeClass("invalid").addClass("valid");
+    } else {
+        $('#capital').removeClass("valid").addClass("invalid");
     }
-}
+    
+    if (password.match(numbers)) {
+        $('#number').removeClass("invalid").addClass("valid");
+    } else {
+        $('#number').removeClass("valid").addClass("invalid");
+    }
+    
+    if (password.length >= 8) {
+        $('#length').removeClass("invalid").addClass("valid");
+    } else {
+        $('#length').removeClass("valid").addClass("invalid");
+    }
+});
 $('#psw').on("focus", function() {
     $("#message").css("display", "block");
 });
 $('#psw').on("blur", function() {
     $("#message").css("display", "none");
 });
-$('#psw').on("keyup", function() {
-    var lowerCaseLetters = /[a-z]/g;
-    var upperCaseLetters = /[A-Z]/g;
-    var numbers = /[0-9]/g;
-    if (password.match(lowerCaseLetters)) {
-        $('#letter').removeClass("invalid").addClass("valid");
+$("#submit").click( function() {
+    var newData = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        password: $('#psw').val(),
+        mobileNumber: $('#mobileNumber').val(),
+        postCode: $("#postCode").val(),
+        area: $('#area').val(),
+        email: $('#email').val(),
+        country: $("#country").val(),
+        stateRegion: $("#stateRegion").val()
     }
-    else {
-        $('#letter').removeClass("valid").addClass("invalid");
-    }
-    if (password.match(upperCaseLetters)) {
-        $('#capital').removeClass("invalid").addClass("valid");
-    }
-    else {
-        $('#capital').removeClass("valid").addClass("invalid");
-    }
-    if (password.match(numbers)) {
-        $('#number').removeClass("invalid").addClass("valid");
-    }
-    else {
-        $('#number').removeClass("valid").addClass("invalid");
-    }
-    if (password.length >= 8) {
-        $('#length').removeClass("invalid").addClass("valid");
-    }
-    else {
-        $('#length').removeClass("valid").addClass("invalid");
-    }   
+    saveData(newData);
 })
-/*function validPassword () {
-    
+function saveData(Data) {
+    localStorage.setItem("FirstName", Data.firstName);
+    localStorage.setItem("lastName", Data.lastName);
+    localStorage.setItem("password", Data.password);
+    localStorage.setItem("email", Data.email);
+    localStorage.setItem("mobileNumber", Data.mobileNumber);
+    localStorage.setItem("country", Data.country);
+    localStorage.setItem("area", Data.area);
+    localStorage.setItem("stateRegion", Data.stateRegion);
+    localStorage.setItem("postCode", Data.postCode);
 }
+
